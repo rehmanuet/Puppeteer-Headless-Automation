a8b1067c67e30c55566a56ab16be867096f1a553
Abdur Rehman <rehmanuet@yahoo.com>
2020-05-12 15:00:07 +0500

    WIP:12-May-20

--- test/tc1.js
+++ test/tc1.js
@@ -1,29 +1,46 @@
 const puppeteer = require('puppeteer');
 const iPhone = puppeteer.devices['iPhone 6'];
-
-
+const screenshot = 'example.png'
+try {
 (async () => {
 
 
-  const browser = await puppeteer.launch({
-
-    headless: false
-  });
-  // const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   // const context = await browser.createIncognitoBrowserContext();
-  // Create a new page in a pristine context.
-  // const page = await context.newPage();
-  // await page.emulate(iPhone);
-  // page.setViewport({
-  //   "width": 1440,
-  //   "height": 10000
-  // })
-  await page.goto('https://google.com');
+
+  page.setViewport({
+    "width": 1440,
+    "height": 10000
+  })
+  await page.goto('https://google.com',{waitUntil:"networkidle2"});
   console.log(browser.isConnected())
+  const query = await page.$("[name='q']")
+  await query.click()
+  await query.type("NorthBay")
+  const search = await page.$("[value='Google Search']")
+
+  await page.waitFor(4000)
+  await search.click()
+
+  await page.waitFor(4000)
+
+  const options = await page.$$eval('div > div > div.r > a > h3', options => options.map(option =>option.textContent));
+  for(i=0;i<options.length;i++){console.log(options[i])}
+  
+  const divCount = await page.$$eval('div > div > div.r > a', divCount => divCount.map(divCount => divCount.textContent)); 
+  for(i=0;i<divCount.length;i++){console.log(divCount[i])}
+
+  await page.screenshot({path: 'example.png'});
+
   await browser.close()
-  // await browser.on('disconnected');
   console.log(browser.isConnected())
+
+
+
 }
 )();
 
+} catch (err) {
+  console.error(err)
+}
---
+++ test/tc1i.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const iPhone = puppeteer.devices['iPhone 6'];
+try {
+(async () => {
+ 
+  const browser = await puppeteer.launch({headless: false});
+  console.log("Browser Status: "+browser.isConnected())
+  const page = await browser.newPage();
+  await page.emulate(iPhone);
+  await page.goto('https://google.com',{waitUntil:"networkidle2"});
+  console.log(browser.isConnected())
+  const query = await page.$("[name='q']")
+  await query.click()
+  await query.type("NETSOL")
+  await page.waitFor(4000)
+  const search = await page.$("[aria-label='Google Search']")
+  await search.click()
+  await page.waitFor(4000)
+  const result= await page.$("#rso > div > div > div > div > div > a > div > div")
+ 
+  const text = await page.evaluate(() => document.querySelector('#rso > div > div > div > div > div > a > div > div').textContent);
+  const text1 = await page.evaluate(() => document.querySelector('#rso > div > div > div > div > div > a > div').textContent);
+  console.log(text)
+  console.log(text1)
+  await result.click()
+  await page.waitForNavigation({waitUntil: 'networkidle0'});
+  
+  
+  console.log(page.url())
+ await browser.close()
+}
+)();
+
+} catch (err) {
+  console.error(err)
+}
